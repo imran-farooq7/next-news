fbb8b1ab8f80a394b4e1111c5214608532367acf
Imran Farooq <imran_farooq7@hotmail.com>
2024-10-09 19:27:23 +0500

    all news action done

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,4 @@
 "use server";
-const NewsAPI = require("newsapi");
-const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
 export const getAllNews = async () => {
 	const res =
 		await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}
